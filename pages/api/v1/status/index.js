202102b69e06c8e316155631505efba9aed9f254
brtgustavo <121704181+brtgustavo@users.noreply.github.com>
2025-09-13 16:37:13 +0000

    add `jsonfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SOMA;");
+  const result = await database.query("SELECT 6 + 1 AS SOMA;");
   console.log(result.rows);
   response.status(200).json({ chave: "Muito bom o curso.dev" });
 }

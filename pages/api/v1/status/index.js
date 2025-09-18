a8848c4238407259a6ad277a5353cf82c868af53
brtgustavo <121704181+brtgustavo@users.noreply.github.com>
2025-09-18 00:42:32 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 6 + 1 AS SOMA;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Muito bom o curso.dev" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  console.log(databaseVersionValue);
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+  console.log(databaseMaxConnectionsValue);
+
+  const dataBaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
+  console.log(databaseOpenedConnectionsValue);
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        maxConnections: parseInt(databaseMaxConnectionsValue),
+        openedConections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,9 +1,17 @@
-// Teste automatizado para verificar o endpoint /api/v1/status
-// Usamos Jest para definir e rodar o teste
 test("GET to /api/v1/status should return 200", async () => {
-  // Faz uma requisição HTTP GET para a API local na porta 3000
   const response = await fetch("http://localhost:3000/api/v1/status");
-  // Verifica se o código de status retornado é 200 (OK)
-  // Caso não seja, o teste falhará
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.maxConnections).toEqual(100);
+
+  expect(responseBody.dependencies.database.openedConections).toEqual(1);
 });
